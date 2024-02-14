3c84429e2d05ff9864d7501ff82b5f80fa56e3a6
minagishl <mvxd568.co@icloud.com>
2024-02-14 16:46:22 +0900

    prisma related fixes

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } }}).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
